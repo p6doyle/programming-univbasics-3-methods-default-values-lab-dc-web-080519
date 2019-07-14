b3dd16c55b3f6c4615dd314cb95bbd23f037df90
Peyton Doyle <p6doyle@gmail.com>
2019-07-14 11:39:15 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,8 @@ veg2 = "sprouts"
 protein = "meat"
 
 def meal_choice(veg1,veg2,protein)
-  if protein = "meat"
-    
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice
